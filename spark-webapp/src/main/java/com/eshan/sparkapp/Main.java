d64325c8f22a76c30ee1b2978b62bc6f28c4e4ec
Eshan Haval <eshan-haval@users.noreply.github.com>
2016-08-11 19:37:27 -0700

    Update Main.java

--- spark-webapp/src/main/java/com/eshan/sparkapp/Main.java
+++ spark-webapp/src/main/java/com/eshan/sparkapp/Main.java
@@ -3,6 +3,7 @@ package com.eshan.sparkapp;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.*;
 
 public class Main{
     
@@ -10,6 +11,13 @@ public class Main{
             port(8081);
             get("/helloStudents",(req,res) -> {
                 return new ModelAndView(null,"index.hbs");
+            }, new HandlebarsTemplateEngine());
+            
+            
+            post("/sign-in",(req,res)->{
+                Map<String, String> modelHashMap = new HashMap<String, String>();
+                modelHashMap.put("username", req.queryParams("username"));
+                return new ModelAndView(modelHashMap,"sign-in.hbs");
             }, new HandlebarsTemplateEngine());
         }
 }
